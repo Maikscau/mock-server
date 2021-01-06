690ba4565240f3210a6cbdf1248a50030cc3d640
麦伟强 <maiweiqiang@sfmail.sf-express.com>
2021-01-06 10:17:35 +0800

    version1.0

--- mocks/static/static.js
+++ mocks/static/static.js
@@ -1,26 +1,28 @@
 const fs = require('fs')
 const path = require('path')
 
+// 静态资源请求
 const getImg = (req, res) => {
 	// 获取请求的文件名
 	const tem = req.url.split('/')
 	const filename = tem[tem.length - 1]
-
+	// 请求的文件在服务器的路径
 	const filePath = path.resolve(__dirname, `../../static/images/${filename}`)
-	
-	// 给客户端返回一个文件流
-
-	//格式必须为 binary，否则会出错
-	// 创建文件可读流
-	const cs = fs.createReadStream(filePath);
-	cs.on("data", chunk => {
-			res.write(chunk);
-	})
-	cs.on("end", () => {
-			res.status(200);
-			res.end();
-	})
-
+	try {
+		// 判断文件是否存在
+		fs.statSync(filePath)
+		// 创建文件可读流
+		const cs = fs.createReadStream(filePath)
+		cs.on('data', chunk => {
+				res.write(chunk)
+		})
+		cs.on('end', () => {
+				res.status(200)
+				res.end()
+		})
+	} catch (e) {
+		res.status(404).json('Not Found')
+	}
 }
 
 module.exports = {
